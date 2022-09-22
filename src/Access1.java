92ab0cbdd31da31841d84455a2a3b09bf40baa82
guhans6 <guhans61101@gmail.com>
2022-09-22 14:06:30 +0530

    Access Modifiers First

---
+++ src/Access1.java
@@ -0,0 +1,13 @@
+//Access Modifiers in Java example
+public  class Access1
+{
+    //private variables
+    private  int a;
+    private  int b;
+
+    protected void display2()
+    {
+        System.out.println("Protected method");
+    }
+    
+}
